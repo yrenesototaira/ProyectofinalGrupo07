9e9d301be15bce56420d7f9e49bc2ec327ad5b16
Carlos Infantas <carlos.infantas@inetum.com>
2025-12-04 03:29:40 -0500

    Gestion de cartas

--- marakos-web/src/environments/environment.ts
+++ marakos-web/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  apiUrlAuth: 'http://localhost:8080/api',
-  apiUrlCustomer: 'http://localhost:8081/api',
+  apiUrl: 'https://proyectofinalgrupo07.onrender.com/api',
+  apiUrlAuth: 'https://proyectofinalgrupo07.onrender.com/api',
+  apiUrlCustomer: 'https://marakos-customer-service.onrender.com/api',
   apiUrlManagement: 'http://localhost:8082/api',
   apiUrlReservation: 'http://localhost:8083/api',
-  apiUrlAdmin: 'http://localhost:8080/api/admin', 
+  apiUrlAdmin: 'https://proyectofinalgrupo07.onrender.com/api/admin', 
   apiUrlCulqi: 'http://localhost:8084/api/payment',
   apiUrlNotification: 'http://localhost:8085/api'
 };
